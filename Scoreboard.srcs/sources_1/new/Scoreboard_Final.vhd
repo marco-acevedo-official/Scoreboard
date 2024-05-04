38f0cd565be8bb158dfe070322cca6f158789056
Marco Acevedo <80277847+marco-acevedo-official@users.noreply.github.com>
2024-05-04 19:36:49 -0400

    Modifying the Scoreboard_Final not finished
    Modifying the Scoreboard_Final not finished. I need to find a way to output to a 2x7Segment display by changing the value of a display select pin every 10 ms

--- Scoreboard.srcs/sources_1/new/Scoreboard_Final.vhd
+++ Scoreboard.srcs/sources_1/new/Scoreboard_Final.vhd
@@ -3,25 +3,29 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 entity Scoreboard_Final is
     Generic (
-        clock_period : time := 10 ns;
-        reg_width : integer := 16
+        clock_period_F : time := 10 ns;
+        reg_width_F : integer := 16
     );
     Port (
         clk_F, rst_F, inc_FR, dec_FR: in std_logic;
-        seg7disp1_F, seg7disp0_F: out unsigned(6 downto 0);
-        bcd1_out_F, bcd0_out_F : out std_logic_vector(3 downto 0);--Debug
-        reg_out_inc_F : out STD_LOGIC_VECTOR(reg_width-1 downto 0);
-        reg_out_dec_F : out STD_LOGIC_VECTOR(reg_width-1 downto 0)
+        pmod : out STD_LOGIC_VECTOR(7 downto 0)
     );
+    --pmod "gfedcbaP" where P is display select
 end Scoreboard_Final;
 
 architecture Connections of Scoreboard_Final is
-	signal inc_f, dec_f : std_logic := '0';
-
+	constant displayTime : time := 20000 ns;
+	signal	inc_f, dec_f : std_logic := '0';
+	signal	seg7disp1_F, seg7disp0_F: unsigned(6 downto 0) := (others => '0');
+	signal	bcd1_out_F, bcd0_out_F : std_logic_vector(3 downto 0) := (others => '0');
+	signal	reg_out_inc_F : STD_LOGIC_VECTOR(reg_width_F-1 downto 0) := (others => '0');
+    signal	reg_out_dec_F : STD_LOGIC_VECTOR(reg_width_F-1 downto 0) := (others => '0');
+    signal intra_pmod : STD_LOGIC_VECTOR(7 downto 0) := (others => '0');
+	
 begin
 
 	INCREASE_Filter : entity work.Button_Filter
-	generic map(clock_period => clock_period, reg_width => reg_width )
+	generic map(clock_period => clock_period_F, reg_width => reg_width_F )
 	port map(
 			clockin => clk_F,
 			resetin => rst_F,
@@ -31,7 +35,7 @@ begin
 	);
 	
 	DECREASE_Filter : entity work.Button_Filter
-	generic map(clock_period => clock_period, reg_width => reg_width )
+	generic map(clock_period => clock_period_F, reg_width => reg_width_F )
 	port map(
 			clockin => clk_F,
 			resetin => rst_F,
@@ -41,7 +45,7 @@ begin
 	);
 	
 	SCOREBOARD : entity work.Scoreboard
-	generic map(clock_period => clock_period, reg_width => reg_width )
+	generic map(clock_period => clock_period_F, reg_width => reg_width_F )
 	port map(
 			clk => clk_F,
 			rst => rst_F,
@@ -53,4 +57,11 @@ begin
 			bcd0_out => bcd0_out_F
 	);
 	
+	alternateDisplay:process
+	begin
+	--pmod "gfedcbaP" where P is display select
+	intra_pmod <= 
+		
+	end process;
+	pmod <= intra_pmod;
 end Connections;
